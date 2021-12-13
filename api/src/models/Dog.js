a854a3824b4eb6031cc05601a845e28942fbb5cb
matiascalvar <matiasncalvar@gmail.com>
2021-12-13 10:49:30 -0300

    working on sequelize models

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,10 +3,29 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('dog', { // change to 'raza'?
+    id: {
+      type: DataTypes.UUID, // creo un id unico para diferenciarlo del id de la API externa
+      defaultValue: DataTypes.UUIDV1, 
+      allowNull: false,
+      primaryKey : true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.TINYINT, // 256 max or string?
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.TINYINT, // 256 max or string?
+      allowNull: false,
+    },
+    // should use camelcase to name tables
+    life_span: {
+      type: DataTypes.TINYINT,
+    },
+    // Agrego un campo image con una imagen por default?
   });
 };
