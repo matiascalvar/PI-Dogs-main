e5eb74576c3eb1f97d2ac95a4332c0e6775e0c3d
matiascalvar <matiasncalvar@gmail.com>
2021-12-22 11:19:11 -0300

    implementing hooks and redux

--- client/src/components/Cards.jsx
+++ client/src/components/Cards.jsx
@@ -1,13 +1,19 @@
+import React, { useEffect } from "react";
+import { connect, useDispatch, useSelector } from "react-redux";
 import Card from "./Card";
-import { connect } from "react-redux";
 import { getBreeds } from "../actions/index";
-function Cards({ breeds }) {
-  let breedsSliced = breeds.slice(0, 8);
-  // console.log(breedsSliced);
+
+function Cards() {
+  const dispatch = useDispatch();
+  const breeds = useSelector((state) => state.breeds);
+
+  useEffect(() => {
+    dispatch(getBreeds());
+  }, [dispatch]);
   return (
     <>
       <h3>Main cards block. Here should render 8 breed cards per page</h3>
-      {breedsSliced.map((e) => (
+      {breeds.map((e) => (
         <Card
           name={e.name}
           weight={e.weight}
@@ -20,6 +26,8 @@ function Cards({ breeds }) {
   );
 }
 
-const mapStateToProps = (state) => ({ breeds: state.breeds });
+// const mapStateToProps = (state) => ({ breeds: state.breeds });
 
-export default connect(mapStateToProps, { getBreeds })(Cards);
+// export default connect(mapStateToProps, { getBreeds })(Cards);
+
+export default Cards;
