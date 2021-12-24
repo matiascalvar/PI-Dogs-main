d51b550cc176c8f6c4914423b97e5f5dcaf4b90d
matiascalvar <matiasncalvar@gmail.com>
2021-12-24 12:32:46 -0300

    working on order methods

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,44 +1,40 @@
 import React from "react";
-import { weightDESC } from "../../actions";
+// import { weightDESC } from "../../actions";
 
-function SearchBar({ orderZA, orderAZ, weightASC, weightDESC }) {
-  // const orderHandlerZA = function (e) {
-  //   orderZA();
-  //   e.preventDefault();
-  // };
-  // const orderHandlerAZ = function (e) {
-  //   orderAZ();
-  //   e.preventDefault();
-  // };
-
-  // Podria hacer un action creator
+function SearchBar({ weightAlpha }) {
   function handleChange(e) {
-    console.log(e.target.value);
+    weightAlpha(e.target.value);
   }
 
   return (
     <>
-      <button onClick={orderAZ} type="submit">
+      <label htmlFor="">
+        <span> Alphabetical Order: </span>
+        <select name="abcorder" id="abcorder" onChange={handleChange}>
+          <option value="ORDER_AZ">A - Z</option>
+          <option value="ORDER_ZA">Z - A</option>
+        </select>
+      </label>
+
+      <label htmlFor="">
+        <span> Weight: </span>
+        <select name="weight" id="weight" onChange={handleChange}>
+          {/* <option value="">Weight Sort</option> */}
+          <option value="WEIGHT_ASC">▼</option>
+          <option value="WEIGHT_DESC">▲</option>
+        </select>
+      </label>
+
+      {/* <button onClick={orderAZ} type="submit">
         A-Z
       </button>
       <button onClick={orderZA} type="submit">
         Z-A
-      </button>
-      <button onClick={weightASC} type="submit">
-        Weight ASC
-      </button>
-      <button onClick={weightDESC} type="submit">
-        Weight DESC
-      </button>
-      <button type="submit">Temperaments</button>
-      <button type="submit">API or DB</button>
-      {/* https://www.w3schools.com/howto/howto_css_switch.asp */}
+      </button> */}
 
-      <select name="weight" id="weight" onChange={handleChange}>
-        <option value="">Weight Sort</option>
-        <option value="ASC">Ascending Order</option>
-        <option value="DESC">Descending Order</option>
-      </select>
+      <button type="submit">Temperaments</button>
+      <button type="submit">API or DB or All</button>
+      {/* https://www.w3schools.com/howto/howto_css_switch.asp */}
     </>
   );
 }
