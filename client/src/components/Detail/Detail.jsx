3cf8054dbeb73e7a52b1c7da9c5442bad9a5c859
matiascalvar <matiasncalvar@gmail.com>
2022-01-01 18:49:46 -0300

    styling up detail

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -30,10 +30,18 @@ function Detail() {
           <h1>{breedDetail.name}</h1>
           <img src={breedDetail.image} alt={breedDetail.name} width="700" />
           <div className={styles.foot}>
-            <p>Height: {breedDetail.height} cm</p>
-            <p>Weight: {breedDetail.weight} kg</p>
-            <p>Temperaments: {breedDetail.temperament}</p>
-            <p>Life Span: {breedDetail.life_span}</p>
+            <p>
+              <span>Height:</span> {breedDetail.height} cm
+            </p>
+            <p>
+              <span>Weight:</span> {breedDetail.weight} kg
+            </p>
+            <p>
+              <span>Temperaments:</span> {breedDetail.temperament}
+            </p>
+            <p>
+              <span>Life Span:</span> {breedDetail.life_span}
+            </p>
           </div>
         </div>
       </>
